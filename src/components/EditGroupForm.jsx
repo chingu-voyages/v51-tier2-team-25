b1a03b78adc3c765ab986bf0f680a3377d61c8cd
Satomi Li <li.satomi0523@gmail.com>
2024-09-18 21:47:48 -0400

    feat: add form element

--- src/components/EditGroupForm.jsx
+++ src/components/EditGroupForm.jsx
@@ -9,6 +9,64 @@ export default function EditGroupForm() {
         <p className="p-0 text-xs text-gray-400">*Mandatory fields</p>
       </div>
 
+      <form
+        className="flex flex-col flex-1 gap-6 overflow-auto border border-none"
+      >
+        <div className="flex flex-col">
+          <div className='flex items-center'>
+            <img src='./public/images/placeholder.jpg' className='border border-none rounded-full w-[80px] h-[80px] mr-4'/>
+            <div className='relative flex flex-col'>
+                <label className="text-sm">
+                Group name*
+                  <input
+                    className="w-full p-2 mt-1 text-left text-gray-500 border border-gray-300 rounded-md h-9"
+                    type="text"
+                    name="name"
+                    // value={groupsData.name}
+                    // onChange={handleChange}
+                    maxLength="30"
+                    required
+                  />
+                </label>
+                {/* {renderGroupId()} */}
+            </div>  
+
+            <label className='ml-2 text-sm'>
+              Allotted budget
+              <input 
+                className='w-full p-2 mt-1 text-left text-gray-500 border border-gray-300 rounded-md h-9'
+                type='number'
+                step='0.01'
+                min='0'
+                name='allottedBudget'
+                // value={groupsData.allottedBudget}
+                // onChange={handleChange}
+                required
+              />
+            </label>
+          </div>
+
+          <label className='flex flex-col pt-4 text-sm '>
+            Group description*
+            <textarea 
+              className='border border-gray-300 rounded-md h-[72px] w-full text-left mt-1 p-2 text-gray-500'              
+              name='description'
+              // value={groupsData.description}
+              // onChange={handleChange}
+              required
+            />
+          </label>
+
+          {/* TODO PLACEHOLDER */}
+          <div className='pt-4 mb-auto'>
+            <p className='text-gray-200'>placeholder</p>
+          </div>
+
+
+        </div>
+        
+      </form>
+
       </div>
     </div>
       );
