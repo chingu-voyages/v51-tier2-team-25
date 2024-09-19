34045ba9d53e15b9cb39475a16a4df51b822edbc
Satomi Li <li.satomi0523@gmail.com>
2024-09-18 21:11:29 -0400

    feat: add an container of edit group form

--- src/components/EditGroupForm.jsx
+++ src/components/EditGroupForm.jsx
@@ -1,8 +1,9 @@
 export default function EditGroupForm() {
 
-
-
-    return (
-        <h1>this is edit form</h1>
+  return (
+    <div className="fixed inset-0 z-50 flex items-center justify-center bg-gray-800 bg-opacity-75">
+      <div className="relative border border-black-100 w-[535px] h-[625px] rounded-md p-6 bg-white flex flex-col m-8 font-geologica">
+      </div>
+    </div>
       );
 }
