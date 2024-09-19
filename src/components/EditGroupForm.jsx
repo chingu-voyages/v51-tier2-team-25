25c80fe27109c865c4acd3c7f312f2816b361617
Satomi Li <li.satomi0523@gmail.com>
2024-09-18 21:19:48 -0400

    feat: add header to editing group form

--- src/components/EditGroupForm.jsx
+++ src/components/EditGroupForm.jsx
@@ -3,6 +3,12 @@ export default function EditGroupForm() {
   return (
     <div className="fixed inset-0 z-50 flex items-center justify-center bg-gray-800 bg-opacity-75">
       <div className="relative border border-black-100 w-[535px] h-[625px] rounded-md p-6 bg-white flex flex-col m-8 font-geologica">
+
+      <div className="flex items-center justify-between pb-4 mb-5 border-b border-black-200">
+        <h1 className="p-0 text-md">Editing Group</h1>
+        <p className="p-0 text-xs text-gray-400">*Mandatory fields</p>
+      </div>
+
       </div>
     </div>
       );
