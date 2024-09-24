595151a9993e939c52e90e09af56781f60ab24f4
Satomi Li <li.satomi0523@gmail.com>
2024-09-23 20:09:54 -0400

    chore: change edit group form name from 'editing group' to 'edit group'

--- src/components/EditGroupForm.jsx
+++ src/components/EditGroupForm.jsx
@@ -99,7 +99,7 @@ export default function EditGroupForm({ group, closeEditGroupFormModal }) {
     <div className="fixed inset-0 z-50 flex items-center justify-center bg-gray-800 bg-opacity-75">
       <div className="relative border border-black-100 w-[535px] h-[625px] rounded-md p-6 bg-white flex flex-col m-8 font-geologica">
         <div className="flex items-center justify-between pb-4 mb-5 border-b border-black-200">
-          <h1 className="p-0 text-md">Editing Group</h1>
+          <h1 className="p-0 text-md">Edit Group</h1>
           <p className="p-0 text-xs text-gray-400">*Mandatory fields</p>
         </div>
 
