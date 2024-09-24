546d3f569e892b29c10fc56b8274322a6d398406
elta79 <elta7679@gmail.com>
2024-09-24 14:46:45 -0400

    In AddExpense.jsx added function to generate date. In Groups.jsx, fixed width to h-[535]

--- src/components/AddGroup.jsx
+++ src/components/AddGroup.jsx
@@ -102,8 +102,8 @@ export default function AddGroup({ closeAddGroupModal }) {
 
   return (
     <div className="fixed inset-0 z-50 flex items-start justify-center bg-gray-800 bg-opacity-75">
-      <div className="relative border border-black-100 w-[535px] h-[625px] rounded-md px-6 pt-6 bg-zinc-50 flex flex-col m-8 font-geologica">
-        <div className="flex items-center justify-between pb-4 mb-5 border-b border-black-200">
+      <div className="relative border w-[535px] h-[625px] rounded-md px-6 pt-6 bg-zinc-50 flex flex-col m-8 font-geologica">
+        <div className="flex items-center justify-between pb-4 mb-5 border-b border-border">
           <h1 className="p-0 text-md">New Group</h1>
           <p className="p-0 text-xs text-gray-400">*Mandatory fields</p>
         </div>
