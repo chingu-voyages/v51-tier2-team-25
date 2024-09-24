69aacd3311c1773c9918dff02ab9c06167af0c33
elta79 <elta7679@gmail.com>
2024-09-24 11:08:25 -0400

    change div size holding members

--- src/components/AddGroup.jsx
+++ src/components/AddGroup.jsx
@@ -102,7 +102,7 @@ export default function AddGroup({ closeAddGroupModal }) {
 
   return (
     <div className="fixed inset-0 z-50 flex items-start justify-center bg-gray-800 bg-opacity-75">
-      <div className="relative border border-black-100 w-[535px] h-[625px] rounded-md px-6 pt-6 bg-zinc-50 flex flex-col m-8 font-geologica ">
+      <div className="relative border border-black-100 w-[535px] h-[625px] rounded-md px-6 pt-6 bg-zinc-50 flex flex-col m-8 font-geologica">
         <div className="flex items-center justify-between pb-4 mb-5 border-b border-black-200">
           <h1 className="p-0 text-md">New Group</h1>
           <p className="p-0 text-xs text-gray-400">*Mandatory fields</p>
