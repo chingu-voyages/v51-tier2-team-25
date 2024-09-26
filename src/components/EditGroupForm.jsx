a0ec25859f1f6526166237e4ded2c206a131b6a6
ivannissimrch <ivannissimrch@gmail.com>
2024-09-25 21:18:02 -0400

    resolve conflicts

--- src/components/EditGroupForm.jsx
+++ src/components/EditGroupForm.jsx
@@ -6,24 +6,15 @@ import toast from "react-hot-toast";
 import GroupTypeSelection from "./GroupTypeSelection";
 import AddMember from "./AddMember";
 import MembersOnGroup from "./MembersOnGroup";
-<<<<<<< HEAD
-import { Link } from "react-router-dom";
-
-export default function EditGroupForm({
-  group,
-  closeEditGroupFormModal,
-  openAddFriendModal,
-}) {
-=======
-import DeleteGroupModal from './DeleteGroupModal';
+import DeleteGroupModal from "./DeleteGroupModal";
 
 export default function EditGroupForm({ group, closeEditGroupFormModal }) {
->>>>>>> 915fda74ff41088f5335f9065059c79d49752e31
   const { updateGroup, deleteGroup } = useContext(AppContext);
   const navigate = useNavigate();
 
   // Do not allow none numeric keys
-  const blockInvalidChar = (e) => ['e','E','+','-'].includes(e.key) && e.preventDefault()
+  const blockInvalidChar = (e) =>
+    ["e", "E", "+", "-"].includes(e.key) && e.preventDefault();
 
   // Temporary state for handling input changes
   const [tempGroupData, setTempGroupData] = useState({
@@ -58,39 +49,27 @@ export default function EditGroupForm({ group, closeEditGroupFormModal }) {
 
     if (newValue === "" && value !== "") {
       toast("Input cannot be empty or contain only spaces");
-<<<<<<< HEAD
-    } else {
-      // Update the state if the input is valid
-      setTempGroupData((prevData) => ({
-        ...prevData,
-        [name]: value,
-      }));
-=======
-      return
-    } 
-    
-    if(type === "number"){
-      const newValue=parseFloat(value)
+      return;
+    }
 
-      if(!isNaN(newValue) && newValue > 1000000){
-        toast("Alloted budget cannot exceed $1,000,000")
-        return
+    if (type === "number") {
+      const newValue = parseFloat(value);
+
+      if (!isNaN(newValue) && newValue > 1000000) {
+        toast("Alloted budget cannot exceed $1,000,000");
+        return;
       }
->>>>>>> 915fda74ff41088f5335f9065059c79d49752e31
     }
     // Update the state if the input is valid
     setTempGroupData((prevData) => ({
       ...prevData,
-      [name]: value, 
+      [name]: value,
     }));
   };
 
   // Handle form submission
   const handleSubmit = (e) => {
     e.preventDefault();
-<<<<<<< HEAD
-    updateGroup(tempGroupData); // Call updateGroup from AppContext
-=======
 
     const budgetRegex = /^(0|[1-9]\d*)(\.\d+)?$/;
 
@@ -99,8 +78,7 @@ export default function EditGroupForm({ group, closeEditGroupFormModal }) {
       return;
     }
 
-    updateGroup(tempGroupData) // Call updateGroup from AppContext
->>>>>>> 915fda74ff41088f5335f9065059c79d49752e31
+    updateGroup(tempGroupData); // Call updateGroup from AppContext
     closeEditGroupFormModal(); // Close the form after saving changes
     toast(`Changes saved`);
   };
@@ -112,7 +90,7 @@ export default function EditGroupForm({ group, closeEditGroupFormModal }) {
   };
 
   // In the render method
-console.log("Current isModalOpen state:", isModalOpen);
+  console.log("Current isModalOpen state:", isModalOpen);
 
   const confirmDelete = () => {
     const groupName = tempGroupData.name;
@@ -142,60 +120,26 @@ console.log("Current isModalOpen state:", isModalOpen);
 
   return (
     <div>
-    <div className="fixed inset-0 z-50 flex items-start justify-center bg-gray-800 bg-opacity-75">
-      <div className="relative  w-[535px] h-[625px] rounded-md px-6 pt-6 bg-zinc-50 flex flex-col m-8 font-geologica overflow-y-auto">
-        <div className="flex items-center justify-between pb-4 mb-5 border-b border-border">
-          <h1 className="p-0 text-md">Edit Group</h1>
-          <p className="p-0 text-xs text-gray-400">*Mandatory fields</p>
-        </div>
+      <div className="fixed inset-0 z-50 flex items-start justify-center bg-gray-800 bg-opacity-75">
+        <div className="relative  w-[535px] h-[625px] rounded-md px-6 pt-6 bg-zinc-50 flex flex-col m-8 font-geologica overflow-y-auto">
+          <div className="flex items-center justify-between pb-4 mb-5 border-b border-border">
+            <h1 className="p-0 text-md">Edit Group</h1>
+            <p className="p-0 text-xs text-gray-400">*Mandatory fields</p>
+          </div>
 
-        <form
-          className="flex flex-col flex-1 gap-6 border-none"
-          onSubmit={handleSubmit}
-        >
-          <div className="flex flex-col">
-<<<<<<< HEAD
-            <div className="flex items-center">
-=======
-            <div className='flex items-start'>
->>>>>>> 915fda74ff41088f5335f9065059c79d49752e31
-              <img
-                src="../images/placeholder.jpg"
-                className="border border-none rounded-full w-[80px] h-[80px] mr-4"
-              />
-              <div className="relative flex flex-col">
-                <label className="text-sm">
-                  Group name*
-<<<<<<< HEAD
-                  <input
-                    className="w-full p-2 mt-1 text-left text-gray-500 border border-gray-300 rounded-md h-9"
-                    type="text"
-                    name="name"
-                    value={tempGroupData.name}
-                    onChange={handleChange}
-                    maxLength="30"
-                    required
-                  />
-                </label>
-                <p className="absolute top-0 p-0 m-0 text-xs text-gray-400 right-8">
-                  #{group.id}
-                </p>
-              </div>
-
-              <label className="ml-2 text-sm">
-                Allotted budget
-                <input
-                  className="w-full p-2 mt-1 text-left text-gray-500 border border-gray-300 rounded-md h-9"
-                  type="number"
-                  step="0.01"
-                  min="0"
-                  name="allottedBudget"
-                  value={tempGroupData.allottedBudget}
-                  onChange={handleChange}
-                  required
+          <form
+            className="flex flex-col flex-1 gap-6 border-none"
+            onSubmit={handleSubmit}
+          >
+            <div className="flex flex-col">
+              <div className="flex items-start">
+                <img
+                  src="../images/placeholder.jpg"
+                  className="border border-none rounded-full w-[80px] h-[80px] mr-4"
                 />
-              </label>
-=======
+                <div className="relative flex flex-col">
+                  <label className="text-sm">
+                    Group name*
                     <input
                       className="w-full p-2 mt-1 text-left border rounded-md text-input-text border-input-border h-9"
                       type="text"
@@ -207,125 +151,97 @@ console.log("Current isModalOpen state:", isModalOpen);
                     />
                   </label>
                   <p className="text-xs text-gray-400">30 character max.</p>
-                  <p className="absolute top-0 p-0 m-0 text-xs text-gray-400 right-8">#{tempGroupData.id}</p>
-              </div>  
+                  <p className="absolute top-0 p-0 m-0 text-xs text-gray-400 right-8">
+                    #{tempGroupData.id}
+                  </p>
+                </div>
 
-              <div className='relative flex flex-col'>
-                  <label className='ml-2 text-sm'>
-                  Allotted budget
-                  <input 
-                    className='w-full p-2 mt-1 text-left border rounded-md text-input-text border-input-border h-9'
-                    type='number'
-                    step={0.01}
-                    min={0.01}
-                    max={1000000}
-                    maxLength={7}   
-                    name='allottedBudget'
-                    value={tempGroupData.allottedBudget}
-                    onChange={handleChange}
-                    onKeyDown={blockInvalidChar}
-                    required
-                  />
-                </label>
-                <p className="ml-2 text-xs text-gray-400">$1,000,000 max.</p>
+                <div className="relative flex flex-col">
+                  <label className="ml-2 text-sm">
+                    Allotted budget
+                    <input
+                      className="w-full p-2 mt-1 text-left border rounded-md text-input-text border-input-border h-9"
+                      type="number"
+                      step={0.01}
+                      min={0.01}
+                      max={1000000}
+                      maxLength={7}
+                      name="allottedBudget"
+                      value={tempGroupData.allottedBudget}
+                      onChange={handleChange}
+                      onKeyDown={blockInvalidChar}
+                      required
+                    />
+                  </label>
+                  <p className="ml-2 text-xs text-gray-400">$1,000,000 max.</p>
+                </div>
               </div>
->>>>>>> 915fda74ff41088f5335f9065059c79d49752e31
-            </div>
 
-            <label className="flex flex-col pt-4 text-sm ">
-              Group description*
-<<<<<<< HEAD
-              <textarea
-                className="border border-gray-300 rounded-md h-[72px] w-full text-left mt-1 p-2 text-gray-500"
-                name="description"
-=======
-              <textarea 
-                className='w-full p-2 mt-1 text-left border rounded-md resize-none text-input-text border-input-border'              
-                name='description'
->>>>>>> 915fda74ff41088f5335f9065059c79d49752e31
-                value={tempGroupData.description}
-                onChange={handleChange}
-                maxLength={350}
-                placeholder="Write your text here."
-                required
-                rows={3}
+              <label className="flex flex-col pt-4 text-sm ">
+                Group description*
+                <textarea
+                  className="w-full p-2 mt-1 text-left border rounded-md resize-none text-input-text border-input-border"
+                  name="description"
+                  value={tempGroupData.description}
+                  onChange={handleChange}
+                  maxLength={350}
+                  placeholder="Write your text here."
+                  required
+                  rows={3}
+                />
+              </label>
+
+              <GroupTypeSelection
+                handleChange={handleChange}
+                groupsData={tempGroupData}
               />
-            </label>
 
-            <GroupTypeSelection
-              handleChange={handleChange}
-              groupsData={tempGroupData} 
-            />
-
-            <AddMember
-              addMemberToGroup={addMemberToGroup}
-              groupMembers={tempGroupData.members}
-            />
-<<<<<<< HEAD
-            <div className="flex items-center justify-between pb-4 mb-4 border-b border-border mt-4">
-              <Link
-                onClick={() => {
-                  closeEditGroupFormModal();
-                  openAddFriendModal();
-                }}
-                className="p-0 text-sm text-gray-400 underline  hover:text-black"
-              >
-                Add new friends to your friend list
-              </Link>
-            </div>
-            <MembersOnGroup
-              groupMembers={tempGroupData.members}
-              deleteMemberFromGroup={deleteMemberFromGroup}
-            />
-=======
-            <div className="pb-12 mt-2 overflow-y-auto max-h-32">
-              <MembersOnGroup
+              <AddMember
+                addMemberToGroup={addMemberToGroup}
                 groupMembers={tempGroupData.members}
-                deleteMemberFromGroup={deleteMemberFromGroup}
               />
-            </div> 
->>>>>>> 915fda74ff41088f5335f9065059c79d49752e31
+              <div className="pb-12 mt-2 overflow-y-auto max-h-32">
+                <MembersOnGroup
+                  groupMembers={tempGroupData.members}
+                  deleteMemberFromGroup={deleteMemberFromGroup}
+                />
+              </div>
 
-            <div className="absolute bottom-0 left-0 right-0 flex items-center w-full p-4 bg-light-indigo place-content-end ">
-              <button
-                type="button"
-                onClick={closeEditGroupFormModal}
-                className="mr-2 text-sm"
-              >
-                Cancel
-              </button>
-              <button
-              type="button"
-                onClick={handleDelete}
-                className="px-3 py-2 mr-2 text-sm bg-red-600 rounded-lg hover:bg-red-800 text-light-indigo"
-              >
-                Delete group
-              </button>
-              <button
-                type="submit"
-                className="px-3 py-2 text-sm rounded-lg hover:bg-hover bg-button text-light-indigo"
-              >
-                Save
-              </button>
+              <div className="absolute bottom-0 left-0 right-0 flex items-center w-full p-4 bg-light-indigo place-content-end ">
+                <button
+                  type="button"
+                  onClick={closeEditGroupFormModal}
+                  className="mr-2 text-sm"
+                >
+                  Cancel
+                </button>
+                <button
+                  type="button"
+                  onClick={handleDelete}
+                  className="px-3 py-2 mr-2 text-sm bg-red-600 rounded-lg hover:bg-red-800 text-light-indigo"
+                >
+                  Delete group
+                </button>
+                <button
+                  type="submit"
+                  className="px-3 py-2 text-sm rounded-lg hover:bg-hover bg-button text-light-indigo"
+                >
+                  Save
+                </button>
+              </div>
             </div>
-          </div>
-        </form>
+          </form>
+        </div>
       </div>
+      <DeleteGroupModal
+        isOpen={isModalOpen}
+        onConfirm={confirmDelete}
+        onCancel={() => setIsModalOpen(false)}
+        groupName={tempGroupData.name}
+      />
     </div>
-<<<<<<< HEAD
   );
 }
-=======
-    <DeleteGroupModal
-      isOpen={isModalOpen}
-      onConfirm={confirmDelete}
-      onCancel={() => setIsModalOpen(false)}
-      groupName={tempGroupData.name}
-    />
-  </div>
-  );
-  }
->>>>>>> 915fda74ff41088f5335f9065059c79d49752e31
 
 EditGroupForm.propTypes = {
   group: PropTypes.object.isRequired,
