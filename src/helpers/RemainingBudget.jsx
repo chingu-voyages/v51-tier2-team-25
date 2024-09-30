2e7d7657db4ebb40ff3e12226ea99947b487b799
elta79 <elta7679@gmail.com>
2024-09-30 12:49:21 -0400

    Update group after expense is deleted so that it will update remaining budget.

--- src/helpers/RemainingBudget.jsx
+++ src/helpers/RemainingBudget.jsx
@@ -6,15 +6,16 @@ const RemainingBudget = ({ groupId }) =>{
 
   const group = groups.find(group => group.id === Number(groupId))
   
-  // console.log("group log expenses:",group.expenses)
+  console.log("group log expenses:", group.expenses)
 
-  const totalExpenses= group.expenses.map(expense => expense.amount)
-    .reduce((sum, expense)=> sum + expense.amount)
+  const totalExpenses = group.expenses
+    .map(expense => Number(expense.amount))
+    .reduce((sum, amount)=> sum + amount, 0)
   
-  // console.log(`totalExp ${group.name}`, totalExpenses)
+  console.log(`totalExp ${group.name}`, totalExpenses)
 
-  const budget = group ? group.allottedBudget : 0
-  // console.log ("Group budget:", budget)
+  const budget = group ? Number(group.allottedBudget) : 0
+  // console.log ("Group budget:", typeof budget)
 
   const remainingBudget = budget - totalExpenses
 
