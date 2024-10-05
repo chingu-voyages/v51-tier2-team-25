8b90b95239eb8b55645780bd7b9205e601ace6c0
Satomi Li <li.satomi0523@gmail.com>
2024-10-05 08:58:43 -0400

    feat: add Expense component to display each expense on Group page

--- src/pages/Expenses.jsx
+++ src/pages/Expenses.jsx
@@ -2,7 +2,7 @@ import { useContext, useState, useEffect } from "react";
 import { useParams } from "react-router-dom"
 import { AppContext } from "../App";
 import EditExpense from "../components/EditExpense";
-import GetOwePaid from "../helpers/GetOwePaid";
+import Expense from "../components/Expense";
 
 export default function Expenses() {
   const { groupId } = useParams()
@@ -52,25 +52,7 @@ export default function Expenses() {
         <>
           {title && <p className="text-sm text-button">{title}</p>}
           {filteredExpenses.map((expense)=>(
-            <div key={expense.id} className="flex items-center justify-between p-4 my-2 border border-gray-300 rounded-md bg-zinc-50">
-              <div className='flex flex-col gap-2'>
-                <div className="flex gap-2 bg-zinc-50">
-                  <p className="text-sm font-bold">{expense.name}</p>
-                  <p className="px-1 text-xs border rounded-md bg-light-indigo border-border">{expense.category}</p>
-                </div>
-                <p className="text-sm">Placeholder for leftover budget</p>
-              </div>
-              <div className='flex gap-4 text-sm'>
-                <div className='flex flex-col gap-2'>
-                  <GetOwePaid />             
-                  <p>Placeholder people remaining</p>
-                </div>
-                <div className="flex gap-2">
-                  <button type="button" className="px-1 rounded-md hover:bg-gray-200" onClick={() => openReceipt}><img src="../../images/Ticket.svg" alt="Ticket" /></button>
-                  <button type="button" className="px-1 rounded-md hover:bg-gray-200" onClick={() => openEditExpense(expense)}><img src="../../images/Edit.svg" alt="Edit" /></button>
-                </div>   
-              </div>
-            </div>
+            <Expense key={expense.id} expense={expense} openReceipt={openReceipt} openEditExpense={openEditExpense}/>
           ))}  
         </>     
       )}
