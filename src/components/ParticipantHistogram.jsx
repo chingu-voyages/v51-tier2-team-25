e8f235960b0e08e1dcb5e34dc4fc15ef2aafd388
BowenAnna <86504623+BowenAnna@users.noreply.github.com>
2024-10-08 00:07:26 -0400

    added the average line to the participants histogram, changed some styling

--- src/components/ParticipantHistogram.jsx
+++ src/components/ParticipantHistogram.jsx
@@ -1,17 +1,45 @@
-import { Bar } from "react-chartjs-2";
+import { Bar} from "react-chartjs-2";
 import PropTypes from "prop-types";
 import calculateOwedAmounts from "../helpers/calculateOwedAmounts";
+import annotationPlugin from 'chartjs-plugin-annotation';
+
+import {
+    Chart as ChartJS,
+    CategoryScale,
+    LinearScale,
+    BarElement,
+    LineElement,
+    PointElement,
+    LineController,
+    Tooltip,
+    Legend,
+  } from 'chart.js';
+  
+  ChartJS.register(
+    CategoryScale,
+    LinearScale,
+    BarElement,
+    LineElement,
+    PointElement,
+    LineController,
+    Tooltip,
+    Legend,
+    annotationPlugin,
+  );
 
 const ParticipantHistogram = ({ group }) => {
-  const owedAmounts = calculateOwedAmounts(group);
+  const { owedAmounts, averageOwed } = calculateOwedAmounts(group);
   console.log("total owed :", owedAmounts);
+  const participantNames = Object.values(owedAmounts).map(o => o.name);
+  const owedValues = Object.values(owedAmounts).map(o => o.totalOwed);
 
   const data = {
-    labels: Object.values(owedAmounts).map(o => o.name),
+    labels: participantNames,
     datasets: [
       {
+        type: 'bar',
         label: "",
-        data: Object.values(owedAmounts).map(o => o.totalOwed),
+        data: owedValues,
         backgroundColor: "#74779C",
         barPercentage: 0.5, // set to 1.0 for no spacing
         categoryPercentage: 0.8, // width of the bar
@@ -76,6 +104,27 @@ const ParticipantHistogram = ({ group }) => {
       legend: {
         display: false,
       },
+      annotation:{
+        annotations:{
+          averageLine:{
+            type:'line',
+            yMin:averageOwed.toFixed(2),
+            yMax:averageOwed.toFixed(2),
+            xMin: -1,
+            xMax:participantNames.length,
+            borderColor:'#A50F0F',
+            borderWidth:1,
+            borderDash:[5,5],
+            label:{
+              font:{
+                family:"Geologica",
+                size:'12',
+                weight:'400',
+              }
+            }
+          }
+        }
+      },
       datalabels: {
         display: true,
           anchor: 'end',
@@ -98,15 +147,17 @@ const ParticipantHistogram = ({ group }) => {
   return (
     <div className="flex flex-col items-center w-full bg-gray-histogram max-w-[785px] max-h-[495px] pt-[24px] pb-[24px] rounded-md border border-border">
       <div className="flex flex-col w-full max-w-[644px] max-h-[447px]">
+        <div className="flex justify-end">
         <div className="flex flex-col w-full px-[6px] py-[4px] mb-6 rounded-md border border-input-border max-w-[175px] max-h-[40px]">
-          <div className="flex items-center">
+          <div className="flex items-center w-[175px] ">
             <span
               className="block w-[30px] h-[2px] mr-2 border-b-2 border-dashed border-red-average w-full my-2"
             ></span>
             <span className="font-geologica text-xs text-modal-text leading-[14px]">
-              Average owed
+              Average owed (US ${averageOwed.toFixed(2)})
             </span>
           </div>
+        </div>
         </div>
         <Bar data={data} options={options} />
       </div>
--- src/helpers/calculateOwedAmounts.jsx
+++ src/helpers/calculateOwedAmounts.jsx
@@ -1,5 +1,7 @@
 const calculateOwedAmounts = (group) => {
     const owedAmounts = {};
+    let totalOwed = 0;
+    let participantCount = 0;
   
     group.expenses.forEach((expense) => {  
       expense.participants.forEach((participant) => {
@@ -11,11 +13,15 @@ const calculateOwedAmounts = (group) => {
         }
         if (!participant.isPaid){
         owedAmounts[participant.id].totalOwed += participant.amountToPay;
+        totalOwed += participant.amountToPay;
+          participantCount++;
         }
       });
     });
   
-    return owedAmounts;
+    const averageOwed = participantCount > 0 ? totalOwed / participantCount : 0;
+
+    return {owedAmounts, averageOwed};
   };
   
   export default calculateOwedAmounts;
